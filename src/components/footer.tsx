09dee752a942e156258e0a1d6df04122a0dcc1e9
hideyuk1-jp <hh11028+github@gmail.com>
2020-11-28 05:39:29 +0900

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import styled from '../components/styled';
+
 const Footer: React.FC<{ title: string }> = ({ title }) => (
-  <footer>
-    © {new Date().getFullYear()}
-    {` `}
-    <Link to={`/`}>{title}</Link>
-  </footer>
+  <FooterTag>
+    <FooterContent className="inner">
+      <Copyright>
+        © {new Date().getFullYear()}
+        {` `}
+        <Link to={`/`}>{title}</Link>
+      </Copyright>
+    </FooterContent>
+  </FooterTag>
 );
 
 export default Footer;
+
+const FooterTag = styled.footer<{}>`
+  background: ${(props) =>
+    props.theme.colors.backgroundAccent};
+`;
+
+const FooterContent = styled.footer<{}>`
+  padding: 40px 0;
+`;
+
+const Copyright = styled.div<{}>`
+  padding: 0 ${(props) => props.theme.sideSpace.base};
+  text-align: center;
+
+  a {
+    color: ${(props) => props.theme.colors.base};
+  }
+`;

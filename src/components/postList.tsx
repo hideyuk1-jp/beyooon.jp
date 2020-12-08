04fff4f98ad95ade0c65e1107a04ba3289cfba31
hideyuk1-jp <hh11028+github@gmail.com>
2020-12-08 19:01:33 +0900

    fix: post design

--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -5,25 +5,39 @@ import PostCard from '../components/postCard';
 import { Posts } from '../types';
 
 const PostListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(
-    auto-fit,
-    minmax(240px, 1fr)
-  );
-  grid-auto-rows: 1fr;
-  grid-gap: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: -12px;
+
+  article {
+    border: 12px solid transparent;
+
+    width: 100%;
+
+    @media screen and (min-width: ${(props) =>
+        props.theme.responsive.medium}) {
+      width: 50%;
+    }
+
+    @media screen and (min-width: ${(props) =>
+        props.theme.responsive.large}) {
+      width: 33.33333%;
+    }
+  }
 `;
 
 const PostList: React.FC<{ posts: Posts }> = ({
   posts,
 }) => (
-  <PostListWrapper className="by-container">
-    {posts.map((node) => {
-      return (
-        <PostCard key={node.fields?.slug} node={node} />
-      );
-    })}
-  </PostListWrapper>
+  <div className="by-container">
+    <PostListWrapper>
+      {posts.map((node) => {
+        return (
+          <PostCard key={node.fields?.slug} node={node} />
+        );
+      })}
+    </PostListWrapper>
+  </div>
 );
 
 export default PostList;
